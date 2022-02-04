fd780b5efdfbd4dc5d6ebba850d5f64a326854ba
AidarSarybayev <aas134@duke.edu>
2022-02-04 03:32:42 -0500

    redoing all this stuff for the 3rd time, hopefully with this it'll be ok.

--- testapp/__init__.py
+++ testapp/__init__.py
@@ -4,6 +4,7 @@ from testapp.github_info_header import github_info_header
 import os
 app_data_path = os.getenv("TESTAPP_DATA_PATH")
 
+"""
 if app_data_path is None:
     raise Exception("You need to add the path to your data directory to the APP_DATA_PATH environmental variable!")
 
@@ -14,4 +15,4 @@ if can_cd_to_data_path != 0:
     raise Exception("Cannot cd to " + app_data_path +
                     ". Please create the directory or double-check your TESTAPP_DATA_PATH entry.")
 
-
+"""
