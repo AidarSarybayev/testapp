69a433a2f49e6326297ea8dd93c0969815c3627d
AidarSarybayev <aas134@duke.edu>
2022-02-07 18:21:30 -0500

    day 2: added family jpeg picture

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Dash(__name__)
 server = app.server
 app.layout = html.Div([
     github_info_header(),
-    html.Img(src="assets/arlan.jpg")
+    html.Img(src="assets/our_family.jpg")
 ])
 
 if __name__ == '__main__':
